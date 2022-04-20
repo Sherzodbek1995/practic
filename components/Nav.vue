2388a56bd67560b91fe1baccf0feea4051741aed
sherzod <shamsiddinovs102@gmail.com>
2022-04-21 00:00:05 +0500

    first comment

---
+++ components/Nav.vue
@@ -0,0 +1,18 @@
+<
+<template>
+  <section class="allNav"></section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <Header />
+    <HomeHeader />
     <Nuxt />
-    <Footer />
+    <HomeFooter />
   </div>
 </template>
 
@@ -10,5 +10,4 @@
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
